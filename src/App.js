a52435fc60118e8f8472c67cc304920b229195bf
nelleeeee <rudghksldl@gmail.com>
2020-08-21 16:29:01 +0900

    timeout

--- src/App.js
+++ src/App.js
@@ -2,37 +2,45 @@ import React from "react";
 
 // 펑션 대신 클래스로 앱 구현
 class App extends React.Component {
+  // state = {
+  //   count: 0,
+  // };
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    // console.log("add");
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
+  // add = () => {
+  //   // console.log("add");
+  //   this.setState((current) => ({ count: current.count + 1 }));
+  // };
 
-  minus = () => {
-    // console.log("minus");
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
+  // minus = () => {
+  //   // console.log("minus");
+  //   this.setState((current) => ({ count: current.count - 1 }));
+  // };
 
   componentDidMount() {
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
   }
 
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
+  // componentDidUpdate() {
+  //   console.log("I just updated");
+  // }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The Number is:{this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
+    // console.log("I'm rendering");
+    // return (
+    //   <div>
+    //     <h1>The Number is:{this.state.count}</h1>
+    //     <button onClick={this.add}>add</button>
+    //     <button onClick={this.minus}>minus</button>
+    //   </div>
+    // );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "loading..." : "ready"}</div>;
   }
 }
 
